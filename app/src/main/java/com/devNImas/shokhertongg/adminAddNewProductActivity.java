b00d791206e0ca346ba9096c72b9ae423e0e711c
NImas <25734873+samin1615088@users.noreply.github.com>
2019-12-19 23:49:59 +0600

    adminAddNewProductActivity is added

--- app/src/main/java/com/devNImas/shokhertongg/adminAddNewProductActivity.java
+++ app/src/main/java/com/devNImas/shokhertongg/adminAddNewProductActivity.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class adminAddNewProductActivity extends AppCompatActivity {
-
+    private  String CategoryName;
+    private Button addNewProductButton;
+    private ImageView inputProductImage;
+    private EditText inputProductName, inputProductDescription, inputProductPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
-        Toast.makeText(adminAddNewProductActivity.this, "adminAddNewProductActivity", Toast.LENGTH_LONG).show();
+
+        CategoryName = getIntent().getExtras().get("category").toString();
+
+        addNewProductButton = findViewById(R.id.add_new_product);
+        inputProductImage = findViewById(R.id.select_product_image);
+        inputProductName = findViewById(R.id.product_name);
+        inputProductDescription = findViewById(R.id.product_describtion);
+        inputProductPrice = findViewById(R.id.product_price);
+
+        
     }
 }

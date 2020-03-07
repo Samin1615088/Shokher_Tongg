5926d383302a688dd3cbdddb130bb42b85d48a6c
NImas <25734873+Samin1615088@users.noreply.github.com>
2020-03-07 13:43:43 +0600

    testCommit

--- app/src/main/java/com/devNImas/shokhertongg/HomeActivity.java
+++ app/src/main/java/com/devNImas/shokhertongg/HomeActivity.java
@@ -61,13 +61,14 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
-        
+
         return true;
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
